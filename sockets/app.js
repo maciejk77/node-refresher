eb0d30ca718943ee38b9fc78e4ac720cc4d33233
Maciej Kurek <maciejk77@gmail.com>
2016-02-13 16:51:04 +0000

    Socket.io

---
+++ sockets/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(client) {
+  console.log('Client connected...');
+});
+
+client.emit('messages', { hello: 'world'} );
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8080);
